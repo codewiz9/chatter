1c5383504752831c407aad709abe20b1362b997c
Ethan Koch <53226197+codewiz9@users.noreply.github.com>
2024-11-18 14:23:13 -0600

    More set up

--- chat/models.py
+++ chat/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+#the goal is to keep all the modles very basic and rely on views and the html
 class Chat(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     message = models.TextField(max_length=500, blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
-class Friends_list(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='owner')
+#modle that contians friedns
+class Friends_List(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='owner')# makes sure that each user only has one firds list bc the one to one feild
     friends = models.ManyToManyField(User, related_name='friends_list')
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Chat, Friends_List
+from django.contrib.auth.models import User
 
-# Create your views here.
+#this view should show part of a users firds list and their most recent chats
+#it should also have ways to accseus the full firnds list and chats
+class dashbaord(generic.TemplateView):
+    pass
+
+class add_friend(generic.UpdateView):
+    pass
+
+class send_message(generic.CreateView):
+    pass
