9b2569d8c57ff3ff7cb540735f7dfdf3e97eeadf
Ethan Koch <53226197+codewiz9@users.noreply.github.com>
2024-11-19 09:45:01 -0600

    Started to set up basic funcionailty

--- chat/models.py
+++ chat/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 #the goal is to keep all the modles very basic and rely on views and the html
 class Chat(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ManyToManyField(User, related_name='recpiants')
     message = models.TextField(max_length=500, blank=False)
     timestamp = models.DateTimeField(auto_now_add=True)
 
--- chat/views.py
+++ chat/views.py
@@ -2,11 +2,23 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Chat, Friends_List
 from django.contrib.auth.models import User
+from django.db.models import Max, F
 
 #this view should show part of a users firds list and their most recent chats
 #it should also have ways to accseus the full firnds list and chats
-class dashbaord(generic.TemplateView):
-    pass
+class DashboardView(generic.ListView):
+    template_name = 'dashboard.html'
+    context_object_name = 'latest_chats'  # More descriptive name for the varribal
+
+    def get_queryset(self):
+       
+        latest_chat_ids = Chat.objects.filter(users=self.request.user).values('users').annotate(latest_chat=Max('id')).values_list('latest_chat', flat=True)
+        latest_chats = Chat.objects.filter(id__in=latest_chat_ids).order_by('-timestamp')
+
+        return latest_chats
+
+
+
 
 class add_friend(generic.UpdateView):
     pass
